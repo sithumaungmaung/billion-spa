5ebf1d50980478ea873c444fd3a370c89a342eed
pyaesonenyein <pyaesonenyein1996@gmail.com>
2026-01-20 17:28:41 +0630

    invoice list & detail

---
+++ app/Filament/Pages/InvoiceDetail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\Invoice;
+use Filament\Pages\Page;
+use App\Models\DailyRoomRecord;
+
+class InvoiceDetail extends Page
+{
+    protected string $view = 'filament.pages.invoice-detail';
+
+    public ?string $invoice_no = null;
+    public $invoiceDetail;
+    public $invoiceRooms;
+
+    protected array $queryString = [
+        'invoice_no',
+    ];
+
+    public function mount(): void
+    {
+
+        $this->invoiceDetail = Invoice::where('invoice_no', $this->invoice_no)
+        ->with('invoiceRooms', 'invoiceProducts')->first();
+
+    }
+}
