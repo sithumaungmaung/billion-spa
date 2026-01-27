cf2e5d33fba275af28156fa0b8b203b441152b4a
learning sithu <learningsithu@gmail.com>
2026-01-27 17:10:58 +0630

    traits

--- resources/views/filament/pages/check-bill.blade.php
+++ resources/views/filament/pages/check-bill.blade.php
@@ -1,7 +1,5 @@
 <x-filament-panels::page>
     <x-filament::section>
-
-
         <div class="flex items-end gap-4 mb-3">
             <div class="flex-1">
                 <x-filament::input.wrapper label="Select Type">
@@ -108,5 +106,68 @@
                 </x-filament::button>
             </div>
         </div>
+    </x-filament::section>
+
+    <!-- system over view -->
+    <x-filament::section>
+        <h3>Room Information</h3>
+        <table class="w-full text-sm text-left border-collapse
+           border border-gray-200 dark:border-white/10 mt-2">
+            <thead class="bg-gray-100 text-gray-700
+                   dark:bg-white/5 dark:text-gray-300">
+                <tr class="border-b border-gray-200 dark:border-white/10">
+                    <th class="px-4 py-3 w-12 text-center">#</th>
+                    <th class="px-4 py-3">Room</th>
+                    <th class="px-4 py-3 text-right">Time</th>
+                    <th class="px-4 py-3 text-right">Therapist</th>
+                    <th class="px-4 py-3 text-right">Service Type</th>
+                    <th class="px-4 py-3 text-right">Service Type Price</th>
+                    <th class="px-4 py-3 text-right">Room Price</th>
+                    <th class="px-4 py-3 text-right">Total</th>
+                </tr>
+            </thead>
+
+            <tbody class="divide-y divide-gray-100
+                   dark:divide-white/10">
+
+                @foreach ($this->systemDailyRecords as $key => $systemDailyRecord)
+                    <tr
+                        class="hover:bg-gray-50 divide-x divide-gray-100 dark:divide-white/10 divide-y
+                            dark:hover:bg-white/5 transition">
+                        <td class="px-4 py-3 text-center text-gray-500">
+                            {{ $key + 1 }}
+                        </td>
+
+                        <td class="px-4 py-3 font-medium">
+                            {{ $systemDailyRecord->room ? $systemDailyRecord->room->name : "" }}
+                        </td>
+
+                        <td class="px-4 py-3 font-medium">
+                            {{ $systemDailyRecord->timeslot ? \Carbon\Carbon::parse($systemDailyRecord->timeslot->start_time)->format('H:i') . " - " . \Carbon\Carbon::parse($systemDailyRecord->timeslot->end_time)->format('H:i')  : "" }}
+                        </td>
+
+                        <td class="px-4 py-3 text-right">
+                            {{ $systemDailyRecord->therapist ? $systemDailyRecord->therapist->name : "" }}
+                        </td>
+
+                        <td class="px-4 py-3 text-right">
+                            {{ $systemDailyRecord->serviceType ? $systemDailyRecord->serviceType->title : "" }}
+                        </td>
+
+                        <td class="px-4 py-3 text-right">
+                            {{ $systemDailyRecord->service_type_price }}
+                        </td>
+
+                        <td class="px-4 py-3 text-right">
+                            {{ $systemDailyRecord->price }}
+                        </td>
+
+                        <td class="px-4 py-3 text-right">
+                            {{ $systemDailyRecord->service_type_price + $systemDailyRecord->price }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </x-filament::section>
 </x-filament-panels::page>
