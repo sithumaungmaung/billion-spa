1fe0bdd0f06cf901f61ff5ba25f6884179d10d86
pyaesonenyein <pyaesonenyein1996@gmail.com>
2026-01-30 16:13:36 +0630

    remove global searchbox

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->globalSearch(false)
             // ->registration()
             ->colors([
                 'primary' => Color::Amber,
